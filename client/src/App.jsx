d9ef4575bf4d3318dfa6b6c9883a5f813af5cf90
Ahmed Emad <ahmedemad56565@gmail.com>
2024-02-07 21:16:00 +0200

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <p className='container'>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+      perspiciatis at quasi veritatis excepturi voluptatibus molestias pariatur
+      sequi voluptates iusto recusandae odit unde, ex blanditiis eos soluta
+      tempora porro officiis ad fuga nam. Ipsa accusamus molestiae rem minima
+      voluptatibus error rerum laborum voluptatum laboriosam. Veniam deserunt
+      voluptatum illum placeat eligendi officia ab odit ratione ex, autem,
+      pariatur laborum exercitationem nihil consectetur earum distinctio.
+      Voluptatibus nostrum in nam quos hic odit, sed illum ad vel perspiciatis
+      sint officia animi consequatur adipisci laudantium error dolorum quod!
+      Recusandae voluptas reprehenderit delectus dolor autem exercitationem
+      suscipit dolore temporibus eum laborum, obcaecati ipsam ipsa! Dicta!
+    </p>
+  );
+}
+
+export default App;
