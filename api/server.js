0e2fa91c8bea14c1a61af30cd93ba17bfccc7c7a
Ahmed Emad <ahmedemad56565@gmail.com>
2024-02-24 19:43:03 +0200

    connecting to mongo DB

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectToDB from './config/db.js';
+import morgan from 'morgan';
 
 const app = express();
-
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 const port = process.env.PORT || 3000;
+connectToDB();
 app.listen(port, () => {
   console.log('Server running on port ' + port);
 });
